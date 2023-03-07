16aa130d97880aeed63e54804564113d2253b86c
Babak Porkarasl <BabakPorkarasl@gmail.com>
2023-03-07 21:01:30 +0330

    5-product-bold-and-product-zoom

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -5,19 +5,19 @@ export default function ProductItem({ product, addToCartHandler }) {
   return (
     <>
       <div className="max-w-2xl mx-auto">
-        <div className="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+        <div className="bg-white hover:shadow-xl rounded-lg max-w-sm flex sm:block dark:bg-gray-800 dark:border-gray-700 p-2">
           <Link href={`/product/${product.slug}`}>
             <img
               // className="rounded-t-lg p-8"
               src={product.image}
               alt={product.name}
-              className="rounded shadow object-cover h-64 w-full"
+              className="rounded shadow object-cover h-28 w-28 sm:h-64 sm:w-full"
             />
           </Link>
 
           <div className="px-5 pb-5">
             <Link href={`/product/${product.slug}`}>
-              <h3 className="text-gray-900 font-semibold text-xl tracking-tight dark:text-white mb-4">
+              <h3 className="text-gray-900 font-semibold text-lg tracking-tight dark:text-white mb-4">
                 {product.name}
               </h3>
             </Link>
--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -1,29 +1,34 @@
 import axios from 'axios';
 import Layout from 'components/Layout';
 import { useRouter } from 'next/router';
- import React, { useContext } from 'react';
- import { toast } from 'react-toastify';
+import React, { useContext } from 'react';
+import { toast } from 'react-toastify';
 import Link from 'next/link';
 import Image from 'next/image';
 import Product from 'models/Product';
 import db from 'utils/db';
 import { Store } from '/utils/Store';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 export default function ProductScreen(props) {
   const { product } = props;
   const { state, dispatch } = useContext(Store);
-  const router= useRouter();
- 
+  const router = useRouter();
+
   if (!product) {
-    return <Layout title="محصول مورد نظر پیدا نشد">مشخصات محصول مورد نظر در دسترس نیست</Layout>;
+    return (
+      <Layout title="محصول مورد نظر پیدا نشد">
+        مشخصات محصول مورد نظر در دسترس نیست
+      </Layout>
+    );
   }
   const addToCartHandler = async () => {
     const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
     const quantity = existItem ? existItem.quantity + 1 : 1;
     const { data } = await axios.get(`/api/products/${product._id}`);
-   
-      if (data.countInStock < quantity) {
-        return toast.error('متاسفانه موجودی محصول پایان یافت');
+
+    if (data.countInStock < quantity) {
+      return toast.error('متاسفانه موجودی محصول پایان یافت');
     }
 
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
@@ -31,29 +36,55 @@ export default function ProductScreen(props) {
   };
   return (
     <Layout title={product.name}>
-      <div className="py-2">
-        <Link href="/">برگشت به صفحه اصلی</Link>
+      <div className="py-2 mb-4 mt-2">
+        <Link className="bg-rose-600 rounded-md text-slate-50 p-2 hover:bg-rose-500" href="/">
+          برگشت به صفحه اصلی
+        </Link>
       </div>
       <div className="grid md:grid-cols-4 md:gap-3">
         <div className="md:col-span-2">
-          <Image
-            src={product.image}
-            alt={product.name}
-            width={520}
-            height={520}
-          ></Image>
+          <TransformWrapper initialScale={1}>
+            {({ zoomIn, zoomOut }) => (
+              <div >
+             
+                <TransformComponent>
+                  <Image
+                    src={product.image}
+                    alt={product.name}
+                    width={520}
+                    height={520}
+                  ></Image>
+                </TransformComponent>
+                   <div className="flex relative mb-8">
+                  <button
+                    className="absolute top-1/2 -mt-4 right-0 w-8 h-8 bg-rose-600 hover:bg-rose-500 rounded-full flex justify-center items-center focus:outline-none "
+                    onClick={() => zoomIn()}
+                  >
+                    <span className="w-5 h-5 text-slate-50 ">+</span>
+                  </button>
+                  <button
+                    className="absolute top-1/2 -mt-4 right-10 w-8 h-8 bg-rose-600 hover:bg-rose-500 rounded-full flex justify-center items-center focus:outline-none"
+                    onClick={() => zoomOut()}
+                  >
+                    <span className="w-5 h-5 text-slate-50">-</span>
+                  </button>
+             
+                </div>
+              </div>
+            )}
+          </TransformWrapper>
         </div>
         <div>
           <ul>
-            <li className='mb-2'>
+            <li className="mb-2">
               <h1 className="text-lg">{product.name}</h1>
             </li>
-            <li className='mb-2'>دسته محصول: {product.category}</li>
-            <li className='mb-2'>برند محصول: {product.brand}</li>
-            <li className='mb-2'><span >توضیحات کوتاه: </span><p>
-               {product.description}
-            </p>
-             </li>
+            <li className="mb-2">دسته محصول: {product.category}</li>
+            <li className="mb-2">برند محصول: {product.brand}</li>
+            <li className="mb-2">
+              <span>توضیحات کوتاه: </span>
+              <p>{product.description}</p>
+            </li>
           </ul>
         </div>
         <div>
@@ -75,9 +106,7 @@ export default function ProductScreen(props) {
           </div>
         </div>
       </div>
-     <div>
-      
-     </div>
+      <div></div>
     </Layout>
   );
 }
